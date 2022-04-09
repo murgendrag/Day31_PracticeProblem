39ef430b4edf5ec1d49c2c10c6ab29ecc6536db6
murgendrag <murgendra67@gmail.com>
2022-04-10 00:02:44 +0530

    create table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,1 +1,13 @@
 create  database AddressBookService
+
+create Table AddressBook
+(
+FirsName varchar(100),
+LastName varchar(100),
+Address varchar(250), 
+City varchar(50),
+State varchar(50), 
+Zip int, 
+Phonenumber int,
+Email varchar(100)
+)
